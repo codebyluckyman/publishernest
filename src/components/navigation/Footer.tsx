a82903cac331ab96f77933260f2fe8556caa4ba3
Wiktor Fila <dosallconnect@gmail.com>
2025-03-14 11:53:54 +1100

    undo footer changes

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="py-4 px-6 bg-gray-50 border-t border-gray-200">
-      <div className="container mx-auto flex justify-right items-right">
+      <div className="container mx-auto flex justify-center items-center">
         <p className="text-sm text-gray-500">
           Copyright © PublishFlow 2025
         </p>
