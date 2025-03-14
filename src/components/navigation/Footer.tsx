473b2bd0ea4c5ef4534fdbebe5526f9962ae41f8
Wiktor Fila <dosallconnect@gmail.com>
2025-03-14 11:51:43 +1100

    updated foort

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="py-4 px-6 bg-gray-50 border-t border-gray-200">
       <div className="container mx-auto flex justify-center items-center">
         <p className="text-sm text-gray-500">
-          Copyright © 2X Advisory 2025
+          Copyright © PublishFlow 2025
         </p>
       </div>
     </footer>
