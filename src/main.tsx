feb0bc6015d9ff1d6ef6d8afcd3ee4ab37cc1211
Wiktor Fila <dosallconnect@gmail.com>
2025-04-09 02:56:55 +0000

    Implement changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 
 import { createRoot } from 'react-dom/client';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AuthProvider } from './context/AuthContext';
-import { OrganizationProvider } from './context/OrganizationProvider';
+import { OrganizationProvider } from './context/OrganizationContext';
 import App from './App.tsx';
 import './index.css';
+import { queryClient } from './lib/react-query';
 
 createRoot(document.getElementById("root")!).render(
-  <AuthProvider>
-    <OrganizationProvider>
-      <App />
-    </OrganizationProvider>
-  </AuthProvider>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <OrganizationProvider>
+        <App />
+      </OrganizationProvider>
+    </AuthProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
